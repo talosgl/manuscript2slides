9d7e484eb2abf63b6f0501168f0ecd981dc3b5e3
talosgl_win11_twr <68163715+talosgl@users.noreply.github.com>
2025-10-01 16:25:26 -0700

    Extract slide creation

---
+++ src/docx2pptx/create_slides.py
@@ -0,0 +1,111 @@
+"""TODO"""
+
+from src.docx2pptx.chunking import is_standard_heading
+from src.docx2pptx.annotations.apply_to_slides import annotate_slide, add_metadata_to_slide_notes
+from src.docx2pptx.run_processing import process_chunk_paragraph_inner_contents
+from src.docx2pptx.models import Chunk_docx
+from src.docx2pptx.utils import debug_print
+from src.docx2pptx import config
+from pptx import presentation
+from docx import document
+from pptx.shapes.placeholder import SlidePlaceholder
+from pptx.slide import Slide, SlideLayout
+from pptx.text.text import TextFrame
+# region create slides from chunks
+
+def slides_from_chunks(
+    doc: document.Document,
+    prs: presentation.Presentation,
+    chunks: list[Chunk_docx],
+) -> None:
+    """Generate slide objects, one for each chunk created by earlier pipeline steps."""
+
+    # Specify which slide layout to use
+    slide_layout = prs.slide_layouts.get_by_name(config.SLD_LAYOUT_CUSTOM_NAME)
+
+    if slide_layout is None:
+        raise KeyError(
+            f"No slide layout found to match provided custom name, {config.SLD_LAYOUT_CUSTOM_NAME}"
+        )
+
+    for chunk in chunks:
+        # Create a new slide for this chunk.
+        new_slide, text_frame = create_blank_slide_for_chunk(prs, slide_layout)
+
+        # Store custom metadata for this chunk that we'll want to tuck into the speaker notes as JSON
+        # (for the purposes of restoring during reverse pipeline runs).
+        slide_metadata = {}
+        headings = []
+        experimental_formatting = []
+
+        # For each paragraph in this chunk, handle adding it
+        for i, paragraph in enumerate(chunk.paragraphs):
+
+            # Creating a new slide and a text frame leaves an empty paragraph in place, even when clearing it.
+            # So if we're at the start of our list, use that existing empty paragraph.
+            if (
+                i == 0
+                and len(text_frame.paragraphs) > 0
+                and not text_frame.paragraphs[0].text
+            ):
+                # Use the existing first/0th paragraph
+                pptx_paragraph = text_frame.paragraphs[0]
+            else:
+                pptx_paragraph = text_frame.add_paragraph()
+
+            # Process the docx's paragraph contents, including both runs & hyperlinks
+            para_experimental_formatting = process_chunk_paragraph_inner_contents(
+                paragraph, pptx_paragraph
+            )
+
+            if para_experimental_formatting:
+                experimental_formatting.extend(para_experimental_formatting)
+
+            if (
+                paragraph.style
+                and paragraph.style.name
+                and is_standard_heading(paragraph.style.name)
+            ):
+                headings.append(
+                    {
+                        "text": paragraph.text.strip(),
+                        "style_id": paragraph.style.style_id,
+                        "name": paragraph.style.name,
+                    }
+                )
+
+        if headings:
+            slide_metadata["headings"] = headings
+        if experimental_formatting:
+            slide_metadata["experimental_formatting"] = experimental_formatting
+
+        notes_text_frame = new_slide.notes_slide.notes_text_frame
+
+        if notes_text_frame is None:
+            raise ValueError(
+                "This slide doesn't seem to have a notes text frame. This should never happen, but it's possible for the notes_slide or notes_text_frame properties to return None if the notes placeholder has been removed from the notes master or the notes slide itself."
+            )
+
+        if config.DISPLAY_DOCX_ANNOTATIONS_IN_SLIDE_SPEAKER_NOTES:
+            annotate_slide(chunk, notes_text_frame)
+
+        if config.PRESERVE_DOCX_METADATA_IN_SPEAKER_NOTES:
+            add_metadata_to_slide_notes(notes_text_frame, chunk, slide_metadata)
+
+def create_blank_slide_for_chunk(
+    prs: presentation.Presentation, slide_layout: SlideLayout
+) -> tuple[Slide, TextFrame]:
+    """Initialize an empty slide so that we can populate it with a chunk."""
+    new_slide = prs.slides.add_slide(slide_layout)
+    content = new_slide.placeholders[1]
+
+    if not isinstance(content, SlidePlaceholder):
+        raise TypeError(f"Expected SlidePlaceholder, got {type(content)}")
+
+    text_frame: TextFrame = content.text_frame
+    text_frame.clear()
+
+    return new_slide, text_frame
+
+
+# endregion
