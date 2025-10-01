52b8ca69762630d302c5a77283f153f635909721
talosgl_win11_twr <68163715+talosgl@users.noreply.github.com>
2025-10-01 16:45:14 -0700

    Move final stuff into main; add __init__.py to module folders

---
+++ src/docx2pptx/__main__.py
@@ -0,0 +1,21 @@
+"""TODO"""
+from __future__ import annotations
+from src.docx2pptx import config
+from src.docx2pptx.utils import debug_print, setup_console_encoding
+from src.docx2pptx import pipeline_docx2pptx
+from src.docx2pptx import pipeline_pptx2docx
+
+def main() -> None:
+    """Entry point for program flow."""
+    setup_console_encoding()
+    debug_print("Hello, manuscript parser!")
+
+    pipeline_docx2pptx.run_docx2pptx_pipeline(config.INPUT_DOCX_FILE)
+
+    # pipeline_pptx2docx.run_pptx2docx_pipeline(INPUT_PPTX_FILE)
+
+
+# region call main
+if __name__ == "__main__":
+    main()
+# endregion
