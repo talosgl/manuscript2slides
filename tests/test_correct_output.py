2accc2aeabca8c9e6fbdfa35e2a81fd2d5bc0e22
talosgl_jotower <68163715+talosgl@users.noreply.github.com>
2025-12-17 17:47:14 -0800

    Fill in the rest of the planned test cases

--- tests/test_correct_output.py
+++ tests/test_correct_output.py
@@ -23,6 +23,19 @@ from tests.helpers import (
 
 
 # region docx2pptx tests
+
+""" 
+Whe converting from sample_doc.docx -> standard pptx output, with these options:
+    - preserve experimental formatting
+    - keep all annotations
+    - preserve metadata in speaker notes
+...the results should match the assertions in the below tests.
+
+# TODO: Consider if we should do similar testing for when options are disabled. 
+# (E.g., experimental formatting on, but speaker notes are empty.)
+"""
+
+
 def test_where_are_data_slide(output_pptx: Path) -> None:
     prs = pptx.Presentation(output_pptx)
     sld_result = find_first_slide_containing(prs, "Where are Data?")
@@ -46,5 +59,59 @@ def test_where_are_data_slide(output_pptx: Path) -> None:
     assert run_result.font.color.rgb == RGBColor_pptx(0xFF, 0x00, 0x00)
     assert run_result.font.italic is True
 
+    # TODO add the other test cases for this slide:
+    """
+    - paragraph text should contain link text from a field code hyperlink, maybe look for https in the string
+	- the speaker notes should contain "COMMENTS FROM SOURCE DOCUMENT" and
+	- "What happens if there's a threaded comment?"
+    """
 
+
+# TODO:
+"""
+
+- The slide containing "by J. King-Yost":
+	- the text should be gray and in italics
+	- the speaker notes should contain:
+```
+START OF JSON METADATA FROM SOURCE DOCUMENT  
+========================================
+
+{  
+ "headings": [  
+ {  
+ "text": "by J. King-Yost",  
+ "style_id": "Heading3",  
+ "name": "Heading 3"  
+ }  
+ ]  
+}
+
+========================================  
+END OF JSON METADATA FROM SOURCE DOCUMENT
+```
+"""
+
+# TODO:
+"""
+In the slide containing a paragraph beginning with: "In a cold concrete underground tunnel"
+- "In a cold concrete underground tunnel" should be a functional hyperlink to https://dataepics.webflow.io/stories/where-are-data
+- "splayed" should be bolded
+- "three dozen directions" should be in italics
+- "buzzing" should be highlighted in yellow
+- "Dust covers the cables" should be in red text
+- "She could wipe them clean without too much risk" should be underlined
+- {!} "read those stories" should be double-underlined - current behavior output is single-underline. I can't remember if this was a limitation we accepted or not: investigate later.
+- Speaker notes should contain:
+	- "COMMENTS FROM SOURCE DOCUMENT:" and "Sample comment"
+	- "FOOTNOTES FROM SOURCE DOCUMENT:" and "1. James Griffiths."
+	- "START OF JSON METADATA FROM SOURCE DOCUMENT" and "experimental_formatting" and YELLOW and Sample Comment and James Griffiths...
+    """
+
+# TODO:
+"""
+The slide containing "Vedantam, Shankar" in the main body of the slide, probably the last slide:
+- speaker notes on this slide should contain "ENDNOTES FROM SOURCE DOCUMENT" and "sample endnote"
+- "START OF JSON METADATA FROM SOURCE DOCUMENT" and "endnotes" and "reference_text"
+"""
 # endregion
