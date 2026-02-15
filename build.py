f771ae810c9a3c1ff6d5f46414a9c1f6a585f79e
zig-m <68163715+talosgl@users.noreply.github.com>
2026-02-14 16:08:07 -0800

    Add platform switching support in build.py for mac & windows

--- build.py
+++ build.py
@@ -1,13 +1,21 @@
-"""Build script for creating Windows binary executable using Nuitka."""
+"""Build script for creating binary executables using Nuitka. Uses platform detection to determine whether to output Mac or Windows binary."""
 
 import subprocess
 import sys
 from pathlib import Path
 
 
+def platform_helper() -> tuple:
+    """Return platform-specific flags for binary builds for macos or windows."""
+    if sys.platform == "darwin":
+        return ("--macos-create-app-bundle", "--macos-app-icon=none") # TODO: Remove second flag if/when we add a cross-platform icon for the app.
+    else:
+        return ("--windows-console-mode=disable",)
+    
+
 def build() -> int:
     """Run Nuitka build command."""
-    print("Building manuscript2slides.exe...")
+    print("Building manuscript2slides...")
     print("This will take 15-20 minutes on first build.\n")
 
     # Nuitka build command
@@ -22,7 +30,7 @@ def build() -> int:
         "--include-package-data=manuscript2slides",
         "--noinclude-qt-translations",
         "--assume-yes-for-downloads",
-        "--windows-console-mode=disable",
+        *platform_helper(),
         "--output-dir=deploy",
         "--output-filename=manuscript2slides",
         str(Path("src") / "manuscript2slides" / "gui.py"),
@@ -33,7 +41,11 @@ def build() -> int:
 
     if result.returncode == 0:
         print("\nPASS Build successful!")
-        print(f"Output: {Path('deploy') / 'gui.dist' / 'manuscript2slides.exe'}")
+        ext = "exe"
+        if sys.platform == "darwin":
+            ext = "app"
+
+        print(f"Output: {Path('deploy') / 'gui.dist' / f'manuscript2slides.{ext}'}")
         print("\nTo distribute: ZIP the entire 'deploy/gui.dist' folder")
     else:
         print("\nFAIL Build failed!")
